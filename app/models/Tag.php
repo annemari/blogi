f5eb1546d8462416ce7d86fd46d6c6862d9f95a8
Anne-Mari Eensaar <55237833+annemari@users.noreply.github.com>
2021-04-01 02:26:21 +0300

    Blogi app

---
+++ app/models/Tag.php
@@ -0,0 +1,28 @@
+<?php
+
+
+class Tag
+{
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = new Database();
+    }
+
+    public function getTags()
+    {
+        $this->db->query('SELECT * FROM tags');
+        $result = $this->db->getAll();
+        return $result;
+    }
+
+    public function getPostTags($id)
+    {
+        $this->db->query('SELECT * FROM tags JOIN post_tags ON tags.id=post_tags.tag_id WHERE post_tags.post_id=:id');
+        $this->db->bind(':id', $id);
+        $result = $this->db->getAll();
+        return $result;
+    }
+
+}

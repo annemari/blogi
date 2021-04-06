1aa9d30f41cd160272e50dd5d052b2badb74d5b3
Anne-Mari Eensaar <55237833+annemari@users.noreply.github.com>
2021-04-06 13:42:33 +0300

    Parandused

--- app/controllers/Tags.php
+++ app/controllers/Tags.php
@@ -3,17 +3,45 @@
 
 class Tags extends Controller
 {
+
     public function __construct()
     {
         $this->tagsModel = $this->model('Tag');
+        $this->postsModel = $this->model('Post');
     }
 
-    public function index()
-    {
+    public function index(){
         $tags = $this->tagsModel->getTags();
         $data = array(
-            'tags' => $tags
+            'tags'=> $tags
         );
         $this->view('tags/index', $data);
     }
+
+    public function show($id)
+    {
+        $tags = $this->tagsModel->getPostTags($id);
+        $posts = $this->postsModel->getPostByTag($id);
+
+        $data = array(
+            'tags' => $tags,
+            'post' => $posts
+        );
+        $this->view('tags/show', $data);
+    }
+
+    public function choice($id)
+    {
+        if (!empty($_POST['otsi'])) {
+        $tag = $this->tagsModel->getTagById($id);
+
+        $data = array(
+            'tag' => $tag
+
+        );
+        $this->view('tags/create', $data);
+    }
+    }
+
+
 }
